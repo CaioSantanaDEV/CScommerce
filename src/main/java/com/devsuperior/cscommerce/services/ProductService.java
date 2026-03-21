1a9e874ddf440efb855962933f2207d0c95c35f6
Caio de Souza Santana <caiosantana.lkd@gmail.com>
2026-03-21 20:34:09 -0300

    GET Products Paged

--- src/main/java/com/devsuperior/cscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/cscommerce/services/ProductService.java
@@ -5,9 +5,13 @@ import com.devsuperior.cscommerce.dto.ProductDTO;
 
 import com.devsuperior.cscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,13 +21,17 @@ public class ProductService {
     private ProductRepository repository;
 
     @Transactional(readOnly = true)
-    public ProductDTO findById(Long id){
+    public ProductDTO findById(Long id) {
         Optional<Product> result = repository.findById(id);
         Product product = result.get();
         return new ProductDTO(product);
+    }
 
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return  result.map(x-> new ProductDTO(x));
 
     }
 }
-
 
